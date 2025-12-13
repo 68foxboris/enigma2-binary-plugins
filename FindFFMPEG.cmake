60a095021d2cd7dda3981d7610fe998c77333963
Boris <boris.fox.solovyev@gmail.com>
2025-12-13 23:04:33 +0200

    Update FindFFMPEG.cmake

--- FindFFMPEG.cmake
+++ FindFFMPEG.cmake
@@ -166,13 +166,13 @@ macro(buildFFMPEG)
 
     BUILD_DEP_TARGET()
 
-    find_program(BASH_COMMAND bash)
-    if(NOT BASH_COMMAND)
-      message(FATAL_ERROR "Internal FFmpeg requires bash.")
-    endif()
+ #  find_program(BASH_COMMAND bash)
+ #  if(NOT BASH_COMMAND)
+ #    message(FATAL_ERROR "Internal FFmpeg requires bash.")
+ #  endif()
     file(WRITE ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/ffmpeg/ffmpeg-link-wrapper
-  "#!${BASH_COMMAND}
-  if [[ $@ == *${APP_NAME_LC}.bin* || $@ == *${APP_NAME_LC}${APP_BINARY_SUFFIX}* || $@ == *${APP_NAME_LC}.so* || $@ == *${APP_NAME_LC}-test* || $@ == *MacOS/Kodi* ]]
+  "#!/bin/bash
+  if [[ $@ == *${APP_NAME_LC}.bin* || $@ == *${APP_NAME_LC}${APP_BINARY_SUFFIX}* || $@ == *${APP_NAME_LC}.so* || $@ == *${APP_NAME_LC}-test* ]]
   then
     avcodec=`PKG_CONFIG_PATH=${DEPENDS_PATH}/lib/pkgconfig ${PKG_CONFIG_EXECUTABLE} --libs --static libavcodec`
     avformat=`PKG_CONFIG_PATH=${DEPENDS_PATH}/lib/pkgconfig ${PKG_CONFIG_EXECUTABLE} --libs --static libavformat`
